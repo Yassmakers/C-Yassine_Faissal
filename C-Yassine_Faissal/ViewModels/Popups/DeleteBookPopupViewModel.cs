f3b5a6f911efc63b32e4e1983f6d6717820c6405
Yassine Messa <89946578+Yassmakers@users.noreply.github.com>
2023-03-23 00:30:04 +0100

    Crud 1/5

--- C-Yassine_Faissal/ViewModels/Popups/DeleteBookPopupViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using C_Yassine_Faissal.ViewModels;
-
-namespace C_Yassine_Faissal.ViewModels.Popups
-{
-    public class DeleteBookPopupViewModel : ViewModelBase
-    {
-        // Add properties and commands for DeleteBookPopupViewModel here
-    }
-}
---
+++ C-Yassine_Faissal/ViewModels/Popups/DeleteItemPopupViewModel.cs
@@ -0,0 +1,37 @@
+﻿using C_Yassine_Faissal.Commands;
+using C_Yassine_Faissal.ViewModels;
+using System.Windows.Input;
+using C_Yassine_Faissal.Models;
+
+
+
+namespace C_Yassine_Faissal.ViewModels.Popups
+    {
+        public class DeleteItemPopupViewModel : ViewModelBase
+    {
+            private string _name;
+
+            public string Name
+            {
+                get => _name;
+                set => SetProperty(ref _name, value);
+            }
+
+            public ICommand CreateUserCommand { get; }
+
+            public DeleteItemPopupViewModel()
+            {
+                CreateUserCommand = new RelayCommand(DeleteItem, CanDeleteItem);
+            }
+
+            private void DeleteItem(object obj)
+            {
+                // Add logic to create a new user
+            }
+
+            private bool CanDeleteItem(object obj)
+            {
+                return !string.IsNullOrEmpty(Name);
+            }
+        }
+    }
